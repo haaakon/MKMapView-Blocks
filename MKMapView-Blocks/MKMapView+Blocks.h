c1f2947fea921bca98481425ea1b23c1da73b150
Håkon Bogen <hakon.bogen@shortcut.no>
2014-01-06 22:10:16 +0100

    adding convenience method definitions for xcode autocomplete

--- MKMapView-Blocks/MKMapView+Blocks.h
+++ MKMapView-Blocks/MKMapView+Blocks.h
@@ -12,88 +12,45 @@
 
 @property (copy, nonatomic) void (^mapViewRegionWillChangeAnimatedBlock)(MKMapView *mapView, BOOL animated);
 @property (copy, nonatomic) void (^mapViewRegionDidChangeAnimatedBlock)(MKMapView *maoView, BOOL animated);
-
 @property (copy, nonatomic) void (^mapViewWillStartLoadingMapBlock)(MKMapView *mapView);
 @property (copy, nonatomic) void (^mapViewDidFinishLoadingMapBlock)(MKMapView *mapView);
 @property (copy, nonatomic) void (^mapViewDidFailLoadingMapBlock)(MKMapView *mapView, NSError *error);
-
 @property (copy, nonatomic) void (^mapViewWillStartRenderingMapBlock)(MKMapView *mapView);
 @property (copy, nonatomic) void (^mapViewDidFinishRenderingMapBlock)(MKMapView *mapView, BOOL fullyRendered);
-
 @property (copy, nonatomic) MKAnnotationView* (^mapViewViewForAnnotationBlock)(MKMapView *mapView, id<MKAnnotation> annotation);
-
 @property (copy, nonatomic) void (^mapViewDidAddAnnotationViewsBlock)(MKMapView *mapView, NSArray *views);
-
 @property (copy, nonatomic) void (^mapViewAnnotationViewCalloutAccessoryControlTappedBlock)(MKMapView *mapView, MKAnnotationView *view , UIControl *control);
-
 @property (copy, nonatomic) void (^mapViewDidSelectAnnotationViewBlock)(MKMapView *mapView, MKAnnotationView *view);
 @property (copy, nonatomic) void (^mapViewDidDeselectAnnotationViewBlock)(MKMapView *mapView, MKAnnotationView *view);
-
 @property (copy, nonatomic) void (^mapViewWillStartLocatingUserBlock)(MKMapView *mapView);
 @property (copy, nonatomic) void (^mapViewDidStopLocatingUserBlock)(MKMapView *mapView);
 @property (copy, nonatomic) void (^mapViewDidUpdateUserLocationBlock)(MKMapView *mapView, MKUserLocation *userLocation);
 @property (copy, nonatomic) void (^mapViewDidFailToLocateUserWithErrorBlock)(MKMapView *mapView, NSError *error);
-
 @property (copy, nonatomic) void (^mapViewAnnotationViewDidChangeDragStateBlock)(MKMapView *mapView,MKAnnotationView *view, MKAnnotationViewDragState newState, MKAnnotationViewDragState oldState);
-
-
 @property (copy, nonatomic) void (^mapViewDidChangeUserTrackingModeBlock)(MKMapView *mapView, MKUserTrackingMode mode, BOOL animated);
-
 @property (copy, nonatomic) MKOverlayRenderer* (^mapViewRendererForOverlayBlock)(MKMapView *mapView,id<MKOverlay> overlay);
 @property (copy, nonatomic) void (^mapViewDidAddOverlayRenderersBlock)(MKMapView *mapView, NSArray *renderers);
 
-
-
-
-- (void)mapView:(MKMapView *)mapView regionWillChangeAnimated:(BOOL)animated;
-- (void)mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated;
-
-- (void)mapViewWillStartLoadingMap:(MKMapView *)mapView;
-- (void)mapViewDidFinishLoadingMap:(MKMapView *)mapView;
-- (void)mapViewDidFailLoadingMap:(MKMapView *)mapView withError:(NSError *)error;
-
-- (void)mapViewWillStartRenderingMap:(MKMapView *)mapView NS_AVAILABLE(10_9, 7_0);
-- (void)mapViewDidFinishRenderingMap:(MKMapView *)mapView fullyRendered:(BOOL)fullyRendered NS_AVAILABLE(10_9, 7_0);
-
-// mapView:viewForAnnotation: provides the view for each annotation.
-// This method may be called for all or some of the added annotations.
-// For MapKit provided annotations (eg. MKUserLocation) return nil to use the MapKit provided annotation view.
-- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation;
-
-// mapView:didAddAnnotationViews: is called after the annotation views have been added and positioned in the map.
-// The delegate can implement this method to animate the adding of the annotations views.
-// Use the current positions of the annotation views as the destinations of the animation.
-- (void)mapView:(MKMapView *)mapView didAddAnnotationViews:(NSArray *)views;
-
-#if TARGET_OS_IPHONE
-// mapView:annotationView:calloutAccessoryControlTapped: is called when the user taps on left & right callout accessory UIControls.
-- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control;
-#endif
-
-- (void)mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)view NS_AVAILABLE(10_9, 4_0);
-- (void)mapView:(MKMapView *)mapView didDeselectAnnotationView:(MKAnnotationView *)view NS_AVAILABLE(10_9, 4_0);
-
-- (void)mapViewWillStartLocatingUser:(MKMapView *)mapView NS_AVAILABLE(10_9, 4_0);
-- (void)mapViewDidStopLocatingUser:(MKMapView *)mapView NS_AVAILABLE(10_9, 4_0);
-- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation NS_AVAILABLE(10_9, 4_0);
-- (void)mapView:(MKMapView *)mapView didFailToLocateUserWithError:(NSError *)error NS_AVAILABLE(10_9, 4_0);
-
-- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view didChangeDragState:(MKAnnotationViewDragState)newState
-   fromOldState:(MKAnnotationViewDragState)oldState NS_AVAILABLE(10_9, 4_0);
-
-#if TARGET_OS_IPHONE
-- (void)mapView:(MKMapView *)mapView didChangeUserTrackingMode:(MKUserTrackingMode)mode animated:(BOOL)animated NS_AVAILABLE(NA, 5_0);
-#endif
-
-- (MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id <MKOverlay>)overlay NS_AVAILABLE(10_9, 7_0);
-- (void)mapView:(MKMapView *)mapView didAddOverlayRenderers:(NSArray *)renderers NS_AVAILABLE(10_9, 7_0);
-
-#if TARGET_OS_IPHONE
-// Prefer -mapView:rendererForOverlay:
-- (MKOverlayView *)mapView:(MKMapView *)mapView viewForOverlay:(id <MKOverlay>)overlay NS_DEPRECATED_IOS(4_0, 7_0);
-// Called after the provided overlay views have been added and positioned in the map.
-// Prefer -mapView:didAddOverlayRenderers:
-- (void)mapView:(MKMapView *)mapView didAddOverlayViews:(NSArray *)overlayViews NS_DEPRECATED_IOS(4_0, 7_0);
-#endif
+// For users convenience so blocks will get auto completed by xcode
+- (void)setMapViewRegionWillChangeAnimatedBlock:(void (^)(MKMapView *mapView, BOOL animated))mapViewRegionWillChangeAnimatedBlock;
+- (void)setMapViewRegionDidChangeAnimatedBlock:(void (^)(MKMapView *mapView, BOOL animated))mapViewRegionDidChangeAnimatedBlock;
+- (void)setMapViewWillStartLoadingMapBlock:(void (^)(MKMapView *mapView))mapViewWillStartLoadingMapBlock;
+- (void)setMapViewDidFinishLoadingMapBlock:(void (^)(MKMapView *mapView))mapViewDidFinishLoadingMapBlock;
+- (void)setMapViewDidFailLoadingMapBlock:(void (^)(MKMapView *mapView, NSError *error))mapViewDidFailLoadingMapBlock;
+- (void)setMapViewWillStartRenderingMapBlock:(void (^)(MKMapView *mapView))mapViewWillStartRenderingMapBlock;
+- (void)setMapViewDidFinishRenderingMapBlock:(void (^)(MKMapView *mapView, BOOL fullyRendered))mapViewDidFinishRenderingMapBlock;
+- (void)setMapViewViewForAnnotationBlock:(MKAnnotationView *(^)(MKMapView *mapView, id<MKAnnotation> annotation))mapViewViewForAnnotationBlock;
+- (void)setMapViewDidAddAnnotationViewsBlock:(void (^)(MKMapView *mapView, NSArray *views))mapViewDidAddAnnotationViewsBlock;
+- (void)setMapViewAnnotationViewCalloutAccessoryControlTappedBlock:(void (^)(MKMapView *mapView, MKAnnotationView *view, UIControl *control))mapViewAnnotationViewCalloutAccessoryControlTappedBlock;
+- (void)setMapViewDidSelectAnnotationViewBlock:(void (^)(MKMapView *mapView, MKAnnotationView *view))mapViewDidSelectAnnotationViewBlock;
+- (void)setMapViewDidDeselectAnnotationViewBlock:(void (^)(MKMapView *mapView, MKAnnotationView *view))mapViewDidDeselectAnnotationViewBlock;
+- (void)setMapViewWillStartLocatingUserBlock:(void (^)(MKMapView *mapView))mapViewWillStartLocatingUserBlock;
+- (void)setMapViewDidStopLocatingUserBlock:(void (^)(MKMapView *mapView))mapViewDidStopLocatingUserBlock;
+- (void)setMapViewDidUpdateUserLocationBlock:(void (^)(MKMapView *mapView, MKUserLocation *userLocation))mapViewDidUpdateUserLocationBlock;
+- (void)setMapViewDidFailToLocateUserWithErrorBlock:(void (^)(MKMapView *mapView, NSError *error))mapViewDidFailToLocateUserWithErrorBlock;
+- (void)setMapViewAnnotationViewDidChangeDragStateBlock:(void (^)(MKMapView *mapView, MKAnnotationView *view, MKAnnotationViewDragState newState, MKAnnotationViewDragState oldState))mapViewAnnotationViewDidChangeDragStateBlock;
+- (void)setMapViewDidChangeUserTrackingModeBlock:(void (^)(MKMapView *mapView, MKUserTrackingMode mode, BOOL animated))mapViewDidChangeUserTrackingModeBlock;
+-(void)setMapViewRendererForOverlayBlock:(MKOverlayRenderer *(^)(MKMapView *mapView, id<MKOverlay> overlay))mapViewRendererForOverlayBlock;
+-(void)setMapViewDidAddOverlayRenderersBlock:(void (^)(MKMapView *mapView, NSArray *renderers))mapViewDidAddOverlayRenderersBlock;
 
 @end
