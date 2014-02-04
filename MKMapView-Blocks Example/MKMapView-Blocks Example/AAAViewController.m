77391a9d3a765de5a639d22aafe03b232f0ad015
Håkon Bogen <hakon.bogen@shortcut.no>
2014-02-04 18:14:28 +0100

    adding more example code

--- MKMapView-Blocks Example/MKMapView-Blocks Example/AAAViewController.m
+++ MKMapView-Blocks Example/MKMapView-Blocks Example/AAAViewController.m
@@ -7,10 +7,10 @@
 //
 
 #import "AAAViewController.h"
+#import "AAAMapAnnotation.h"
 
 @interface AAAViewController ()
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
-
 @end
 
 @implementation AAAViewController
@@ -19,9 +19,11 @@
 {
     [super viewDidLoad];
     self.mapView.delegate = self;
+    [self.mapView setShowsUserLocation:YES];
     [self.mapView setMapViewDidFinishLoadingMapBlock:^(MKMapView *mapView) {
         NSLog(@"did finish loading map");
     }];
+    
     [self.mapView setMapViewWillStartLoadingMapBlock:^(MKMapView *mapView) {
         NSLog(@"will start loading map");
         
@@ -30,7 +32,38 @@
     [self.mapView setMapViewDidFailLoadingMapBlock:^(MKMapView *mapView, NSError *error) {
         NSLog(@"failed with error %@",error);
     }];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self.mapView setMapViewDidUpdateUserLocationBlock:^(MKMapView *mapView, MKUserLocation *userLocation) {
+        NSLog(@"did update user location");
+    }];
+    
+    [self.mapView setMapViewDidFinishRenderingMapBlock:^(MKMapView *mapView, BOOL fullyRendered) {
+        
+    }];
+
+    [self.mapView setMapViewViewForAnnotationBlock:^MKAnnotationView *(MKMapView *mapView, id<MKAnnotation> annotation) {
+          if ([annotation isKindOfClass:[MKUserLocation class]]) {
+              return nil;
+          }
+        MKPinAnnotationView *annotationView = (MKPinAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:@"mapAnnotation"];
+        if (annotationView == nil) {
+            annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"mapAnnotation"];
+        }
+        annotationView.pinColor = MKPinAnnotationColorGreen;
+        return annotationView;
+    }];
+    
+    NSMutableArray *mutableAnnotations = [NSMutableArray array];
+    for (NSInteger i = 0; i < 30; i++) {
+        AAAMapAnnotation *annotation = [[AAAMapAnnotation alloc ] init];
+        CLLocationDegrees longitude = arc4random() % 80;
+        CLLocationDegrees latitude =  arc4random() % 80;
+        CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake( longitude ,  latitude);
+        annotation.coordinate = coordinate;
+        [mutableAnnotations addObject:annotation];
+    }
+    
+    [self.mapView addAnnotations:mutableAnnotations];
 }
 
 
